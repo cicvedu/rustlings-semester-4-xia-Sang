2cf05542c107b998b209e60ecfae20a7dad08a1e
xia-sang <3188674636@qq.com>
2024-07-07 14:12:03 +0800

    ch1

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
@@ -25,9 +23,13 @@ pub struct ReportCard {
 }
 
 impl ReportCard {
-    pub fn print(&self) -> String {
+    pub fn print1(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+    pub fn print2(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, "A+")
     }
 }
 
@@ -43,7 +45,7 @@ mod tests {
             student_age: 12,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print1(),
             "Tom Wriggle (12) - achieved a grade of 2.1"
         );
     }
@@ -57,7 +59,7 @@ mod tests {
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print2(),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
